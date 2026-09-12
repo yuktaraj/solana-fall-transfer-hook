b704f449d7619ee1408bf1a0bc20d45996a8c1ef
yukta raj <yuktaraj@yuktas-MacBook-Air.local>
2026-09-12 11:48:36 +0530

    challenge 1

--- programs/solana-fall-transfer-hook/src/lib.rs
+++ programs/solana-fall-transfer-hook/src/lib.rs
@@ -11,7 +11,7 @@ pub use constants::*;
 pub use instructions::*;
 pub use state::*;
 
-declare_id!("2JfEsBs1CWdqsrgrPzqZywenRgjTP1cRwuybYyRtccuM");
+declare_id!("CP5ZtgWEbUnSL1FBp649UjwWV28Ut8y3hAnWWq1kRpHY");
 
 #[program]
 pub mod solana_fall_transfer_hook {
